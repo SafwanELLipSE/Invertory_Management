fef67192a5bf5c4dea751ee3fce4b84e8d2f43a4
K M Safwan Hassan <safwanhassan13@gmail.com>
2020-08-14 21:21:26 +0600

    graph coomplete for order daily

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -12,6 +12,7 @@ use App\User;
 use App\Order;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 class HomeController extends Controller
 {
@@ -44,30 +45,33 @@ class HomeController extends Controller
           $today = Order::whereDate('created_at', '=',date('Y-m-d'))->count();
           $yesterday = Order::whereDate('created_at', '=', date('Y-m-d',strtotime('-1 days')) )->count();
           $lastWeek = Order::whereDate('created_at', '>', date('Y-m-d',strtotime('-7 days')) )->count();
-          $lastMonth = Order::whereDate('created_at', '>', date('Y-m-d',strtotime('-30 days')) )->count();
           $lastYear = Order::whereDate('created_at', '>', date('Y-m-d',strtotime('-365 days')) )->count();
 
-          $hours4 = Order::whereDate('created_at', '=',date('Y-m-d'))->whereDate('created_at', '=',strtotime('-4 hours'))->count();
-          $hours8 = Order::whereDate('created_at', '=',date('Y-m-d'))->whereDate('created_at', '=',strtotime('-8 hours'))->count();
-          $hours12 = Order::whereDate('created_at', '=',date('Y-m-d'))->whereDate('created_at', '=',strtotime('-12 hours'))->count();
-          $hours16 = Order::whereDate('created_at', '=',date('Y-m-d'))->whereDate('created_at', '=',strtotime('-16 hours'))->count();
-          $hours20 = Order::whereDate('created_at', '=',date('Y-m-d'))->whereDate('created_at', '=',strtotime('-20 hours'))->count();
-          $hours24 = Order::whereDate('created_at', '=',date('Y-m-d'))->whereDate('created_at', '=',strtotime('-24 hours'))->count();
+          $date = new Carbon;
+          $chart = new OrderChart;
+          $times = array();
+          $hours = array();
+          $count = 0;
+          for($i=0; $i<=24;$i++){
+              $times[] = $count;
+              $hours[] = Order::whereDate('created_at', '=',date('Y-m-d'))->where('created_at', '>=', $date->subHours($count))->count();
+              $count++;
+          }
+
+          $total_times = implode(",",$times);
+
+          $chart->labels(['0','4','8','12','16','20','24']);
+          $chart->dataset('Today\'s Order','line',$hours)
+          ->backgroundColor('rgba(112, 195, 250, 0.93)')->color('rgba(16, 20, 148, 1)');
+
         }
         if(Auth::user()->isEmployee())
         {
             $today = Order::where('created_by',Auth::user()->id)->whereDate('created_at', '=',date('Y-m-d'))->count();
             $yesterday = Order::where('created_by',Auth::user()->id)->whereDate('created_at', '=', date('Y-m-d',strtotime('-1 days')) )->count();
             $lastWeek = Order::where('created_by',Auth::user()->id)->whereDate('created_at', '>', date('Y-m-d',strtotime('-7 days')) )->count();
-            $lastMonth = Order::where('created_by',Auth::user()->id)->whereDate('created_at', '>', date('Y-m-d',strtotime('-30 days')) )->count();
             $lastYear = Order::where('created_by',Auth::user()->id)->whereDate('created_at', '>', date('Y-m-d',strtotime('-365 days')) )->count();
         }
-
-        $chart = new OrderChart;
-
-        $chart->labels(['0','4','8','12','16','20','24']);
-        $chart->dataset('Today\'s Order','line',[0,$hours4,$hours8,$hours12,$hours16,$hours20,$hours24])->backgroundColor('rgba(112, 195, 250, 0.93)');
-
 
 
         return view('home',[
